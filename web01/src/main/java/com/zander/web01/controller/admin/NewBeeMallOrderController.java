caf21de0339bebac48e8ec3aa10641cb818579f3
weizhiqiang <weizhiqiang@itcast.cn>
2021-05-28 11:41:52 +0800

    会员用户管理+后台订单价格修改/配货/出库/关闭订单处理+删除不需要的测试文件+验证码修改

---
+++ web01/src/main/java/com/zander/web01/controller/admin/NewBeeMallOrderController.java
@@ -0,0 +1,139 @@
+package com.zander.web01.controller.admin;
+
+import com.zander.web01.common.ServiceResultEnum;
+import com.zander.web01.controller.vo.NewBeeMallOrderItemVO;
+import com.zander.web01.bean.NewBeeMallOrder;
+import com.zander.web01.service.NewBeeMallOrderService;
+import com.zander.web01.util.PageQueryUtil;
+import com.zander.web01.util.Result;
+import com.zander.web01.util.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author 13
+ * @qq交流群 796794009
+ * @email 2449207463@qq.com
+ * @link https://github.com/newbee-ltd
+ */
+@Controller
+@RequestMapping("/admin")
+public class NewBeeMallOrderController {
+
+    @Resource
+    private NewBeeMallOrderService newBeeMallOrderService;
+
+    @GetMapping("/orders")
+    public String ordersPage(HttpServletRequest request) {
+        request.setAttribute("path", "orders");
+        return "admin/newbee_mall_order";
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping(value = "/orders/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (StringUtils.isEmpty(params.get("page")) || StringUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(newBeeMallOrderService.getNewBeeMallOrdersPage(pageUtil));
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping(value = "/orders/update", method = RequestMethod.POST)
+    @ResponseBody
+    public Result update(@RequestBody NewBeeMallOrder newBeeMallOrder) {
+        if (Objects.isNull(newBeeMallOrder.getTotalPrice())
+                || Objects.isNull(newBeeMallOrder.getOrderId())
+                || newBeeMallOrder.getOrderId() < 1
+                || newBeeMallOrder.getTotalPrice() < 1
+                || StringUtils.isEmpty(newBeeMallOrder.getUserAddress())) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = newBeeMallOrderService.updateOrderInfo(newBeeMallOrder);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 详情
+     */
+    @GetMapping("/order-items/{id}")
+    @ResponseBody
+    public Result info(@PathVariable("id") Long id) {
+        List<NewBeeMallOrderItemVO> orderItems = newBeeMallOrderService.getOrderItems(id);
+        if (!CollectionUtils.isEmpty(orderItems)) {
+            return ResultGenerator.genSuccessResult(orderItems);
+        }
+        return ResultGenerator.genFailResult(ServiceResultEnum.DATA_NOT_EXIST.getResult());
+    }
+
+    /**
+     * 配货
+     */
+    @RequestMapping(value = "/orders/checkDone", method = RequestMethod.POST)
+    @ResponseBody
+    public Result checkDone(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = newBeeMallOrderService.checkDone(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 出库
+     */
+    @RequestMapping(value = "/orders/checkOut", method = RequestMethod.POST)
+    @ResponseBody
+    public Result checkOut(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = newBeeMallOrderService.checkOut(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 关闭订单
+     */
+    @RequestMapping(value = "/orders/close", method = RequestMethod.POST)
+    @ResponseBody
+    public Result closeOrder(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = newBeeMallOrderService.closeOrder(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+
+}
--- web01/src/main/java/com/zander/web01/controller/common/CommonController.java
+++ web01/src/main/java/com/zander/web01/controller/common/CommonController.java
@@ -57,15 +57,15 @@ public class CommonController {
     public void mallKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         com.google.code.kaptcha.impl.DefaultKaptcha newBeeMallLoginKaptcha = new com.google.code.kaptcha.impl.DefaultKaptcha();
         Properties properties = new Properties();
-        properties.put("kaptcha.border", "no");
-        properties.put("kaptcha.textproducer.font.color", "27,174,171");
-        properties.put("kaptcha.noise.color", "20,33,42");
-        properties.put("kaptcha.textproducer.font.size", "30");
-        properties.put("kaptcha.image.width", "110");
-        properties.put("kaptcha.image.height", "40");
+//        properties.put("kaptcha.border", "no");
+//        properties.put("kaptcha.textproducer.font.color", "27,174,171");
+//        properties.put("kaptcha.noise.color", "20,33,42");
+//        properties.put("kaptcha.textproducer.font.size", "30");
+//        properties.put("kaptcha.image.width", "110");
+//        properties.put("kaptcha.image.height", "40");
         properties.put("kaptcha.session.key", Constants.MALL_VERIFY_CODE_KEY);
-        properties.put("kaptcha.textproducer.char.space", "2");
-        properties.put("kaptcha.textproducer.char.length", "6");
+//        properties.put("kaptcha.textproducer.char.space", "2");
+//        properties.put("kaptcha.textproducer.char.length", "6");
         Config config = new Config(properties);
         newBeeMallLoginKaptcha.setConfig(config);
         byte[] captchaOutputStream = null;
@@ -91,4 +91,3 @@ public class CommonController {
         responseOutputStream.close();
     }
 }
-
